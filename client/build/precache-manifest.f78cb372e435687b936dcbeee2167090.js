56b27ec594bf02f4f5f97b8b4f322fd089bacc0e
Mohammed Minhaz <mminhaz93@gmail.com>
2020-02-06 22:22:32 -0500

    build

---
+++ client/build/precache-manifest.f78cb372e435687b936dcbeee2167090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f761ea298511c038f39e90d5956a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea12afdbd619f398dee0",
+    "url": "/static/css/main.c33d4dc4.chunk.css"
+  },
+  {
+    "revision": "7be2fd84205b19d61314",
+    "url": "/static/js/2.15702d44.chunk.js"
+  },
+  {
+    "revision": "ea12afdbd619f398dee0",
+    "url": "/static/js/main.dc8e0df4.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
